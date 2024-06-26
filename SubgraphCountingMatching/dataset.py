48a873daa88b133047928699ed7c24af71ad4425
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-06-27 01:00:16 +0700

    Fix bug

--- SubgraphCountingMatching/dataset.py
+++ SubgraphCountingMatching/dataset.py
@@ -1494,7 +1494,7 @@ class GraphAdjDataset(Dataset):
                     x["subisomorphisms"] = th.from_numpy(np.expand_dims(
                         np.array(mapping).T[1], 0))
                 else:
-                    x["subisomorphisms"] = th.tensor([])
+                    x["subisomorphisms"] = th.from_numpy(np.array(mapping, dtype=np.int64))
                 x["counts"] = 1
 
                 x["g_len"] = len(x["graph"])
