6956367ba4f63fb741599eccc27f82d771930e60
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-07-11 17:10:15 +0700

    Add script reading results

---
+++ SubgraphCountingMatching/read_results.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import pickle
+import pandas as pd
+
+
+result_file = sys.argv[1]
+subgraph_size_file = os.path.join(sys.argv[2], "subgraphs_size.pkl")
+subgraph_degree_file = os.path.join(sys.argv[2], "subgraphs_degree.pkl")
+
+df = pd.read_json(result_file)
+df = df.to_dict()
+
+results = {
+    "Execution Time": df["time"]["total"]/len(df["time"]["avg"]),
+    "ROC AUC": df["error"]["AUC"],
+    "PR AUC": df["error"]["PR"],
+    "Precision": df["error"]["PREC"],
+    "Recall": df["error"]["RECALL"],
+    "F1-Score": df["error"]["F1"],
+    "Accuracy": df["error"]["ACC"],
+}
+
+
+def get_avg_runtime(sg_list, all_sg_list, all_runtime):
+    runtime = 0
+    for i, sg in enumerate(all_sg_list):
+        if sg in sg_list:
+            runtime += all_runtime[i]
+    return runtime/len(sg_list)
+
+subgraph_size_dict = pickle.load(open(subgraph_size_file, "rb"))
+subgraph_degree_dict = pickle.load(open(subgraph_degree_file, "rb"))
+
+## Non-dense (degree <= 3)
+nondense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys() if subgraph_degree_dict[subgraph] <= 3]
+# size <= 20
+nondense_ss20 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] <= 20]
+# 20 < size <= 40
+nondense_ss40 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 20 and subgraph_size_dict[subgraph] <= 40]
+# 40 < size <= 60
+nondense_ss60 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 40 and subgraph_size_dict[subgraph] <= 60]
+# size > 60
+nondense_ss60p = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 60]
+
+results["Non-Dense Subgraphs"] = {
+    "All": get_avg_runtime(nondense_subgraphs, df["data"]["id"], df["time"]["avg"]),
+    "Size <= 20": get_avg_runtime(nondense_ss20, df["data"]["id"], df["time"]["avg"]),
+    "20 < Size <= 40": get_avg_runtime(nondense_ss40, df["data"]["id"], df["time"]["avg"]),
+    "40 < Size <= 60": get_avg_runtime(nondense_ss60, df["data"]["id"], df["time"]["avg"]),
+    "Size > 60": get_avg_runtime(nondense_ss60p, df["data"]["id"], df["time"]["avg"]),
+}
+
+## Dense (degree > 3)
+dense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys() if subgraph_degree_dict[subgraph] > 3]
+# size <= 20
+dense_ss20 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] <= 20]
+# 20 < size <= 40
+dense_ss40 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 20 and subgraph_size_dict[subgraph] <= 40]
+# 40 < size <= 60
+dense_ss60 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 40 and subgraph_size_dict[subgraph] <= 60]
+# size > 60
+dense_ss60p = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 60]
+
+results["Dense Subgraphs"] = {
+    "All": get_avg_runtime(dense_subgraphs, df["data"]["id"], df["time"]["avg"]),
+    "Size <= 20": get_avg_runtime(dense_ss20, df["data"]["id"], df["time"]["avg"]),
+    "20 < Size <= 40": get_avg_runtime(dense_ss40, df["data"]["id"], df["time"]["avg"]),
+    "40 < Size <= 60": get_avg_runtime(dense_ss60, df["data"]["id"], df["time"]["avg"]),
+    "Size > 60": get_avg_runtime(dense_ss60p, df["data"]["id"], df["time"]["avg"]),
+}
+
+print(results)
