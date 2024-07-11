f44cf71eb0b4d4feddd4303065a322baa2088e81
Martin Nguyen <nguyenquangduc2000@gmail.com>
2024-07-11 21:14:02 +0700

    Fix bug

--- SubgraphCountingMatching/read_results.py
+++ SubgraphCountingMatching/read_results.py
@@ -23,6 +23,7 @@ results = {
 
 print(results)
 
+
 def get_avg_runtime(sg_list, all_sg_list, all_runtime):
     runtime = 0
     # for i, sg in enumerate(all_sg_list):
@@ -33,43 +34,58 @@ def get_avg_runtime(sg_list, all_sg_list, all_runtime):
         runtime += all_runtime[all_sg_list.index(sg)]
     return runtime/len(sg_list)
 
+
 subgraph_size_dict = pickle.load(open(subgraph_size_file, "rb"))
 subgraph_degree_dict = pickle.load(open(subgraph_degree_file, "rb"))
 
-## Non-dense (degree <= 3)
-nondense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys() if subgraph_degree_dict[subgraph] <= 3]
+# Non-dense (degree <= 3)
+nondense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys(
+) if subgraph_degree_dict[subgraph] <= 3]
 # size <= 20
-nondense_ss20 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] <= 20]
+nondense_ss20 = [
+    subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] <= 20]
 # 20 < size <= 40
-nondense_ss40 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 20 and subgraph_size_dict[subgraph] <= 40]
+nondense_ss40 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph]
+                 > 20 and subgraph_size_dict[subgraph] <= 40]
 # 40 < size <= 60
-nondense_ss60 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 40 and subgraph_size_dict[subgraph] <= 60]
+nondense_ss60 = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph]
+                 > 40 and subgraph_size_dict[subgraph] <= 60]
 # size > 60
-nondense_ss60p = [subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 60]
+nondense_ss60p = [
+    subgraph for subgraph in nondense_subgraphs if subgraph_size_dict[subgraph] > 60]
 
-results["Non-Dense Subgraphs"] = {
-    "Size <= 20": get_avg_runtime(nondense_ss20, df["data"]["id"], df["time"]["avg"]),
-    "20 < Size <= 40": get_avg_runtime(nondense_ss40, df["data"]["id"], df["time"]["avg"]),
-    "40 < Size <= 60": get_avg_runtime(nondense_ss60, df["data"]["id"], df["time"]["avg"]),
-    "Size > 60": get_avg_runtime(nondense_ss60p, df["data"]["id"], df["time"]["avg"]),
-}
+print("Non-dense subgraphs")
+print("Size <= 20", get_avg_runtime(
+    nondense_ss20, df["data"]["id"], df["time"]["avg"]))
+print("20 < Size <= 40", get_avg_runtime(
+    nondense_ss40, df["data"]["id"], df["time"]["avg"]))
+print("40 < Size <= 60", get_avg_runtime(
+    nondense_ss60, df["data"]["id"], df["time"]["avg"]))
+print("Size > 60", get_avg_runtime(
+    nondense_ss60p, df["data"]["id"], df["time"]["avg"]))
 
-## Dense (degree > 3)
-dense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys() if subgraph_degree_dict[subgraph] > 3]
+# Dense (degree > 3)
+dense_subgraphs = [subgraph for subgraph in subgraph_size_dict.keys(
+) if subgraph_degree_dict[subgraph] > 3]
 # size <= 20
-dense_ss20 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] <= 20]
+dense_ss20 = [
+    subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] <= 20]
 # 20 < size <= 40
-dense_ss40 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 20 and subgraph_size_dict[subgraph] <= 40]
+dense_ss40 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph]
+              > 20 and subgraph_size_dict[subgraph] <= 40]
 # 40 < size <= 60
-dense_ss60 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 40 and subgraph_size_dict[subgraph] <= 60]
+dense_ss60 = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph]
+              > 40 and subgraph_size_dict[subgraph] <= 60]
 # size > 60
-dense_ss60p = [subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 60]
+dense_ss60p = [
+    subgraph for subgraph in dense_subgraphs if subgraph_size_dict[subgraph] > 60]
 
-results["Dense Subgraphs"] = {
-    "Size <= 20": get_avg_runtime(dense_ss20, df["data"]["id"], df["time"]["avg"]),
-    "20 < Size <= 40": get_avg_runtime(dense_ss40, df["data"]["id"], df["time"]["avg"]),
-    "40 < Size <= 60": get_avg_runtime(dense_ss60, df["data"]["id"], df["time"]["avg"]),
-    "Size > 60": get_avg_runtime(dense_ss60p, df["data"]["id"], df["time"]["avg"]),
-}
-
-print(results)
+print("Dense subgraphs")
+print("Size <= 20", get_avg_runtime(
+    dense_ss20, df["data"]["id"], df["time"]["avg"]))
+print("20 < Size <= 40", get_avg_runtime(
+    dense_ss40, df["data"]["id"], df["time"]["avg"]))
+print("40 < Size <= 60", get_avg_runtime(
+    dense_ss60, df["data"]["id"], df["time"]["avg"]))
+print("Size > 60", get_avg_runtime(
+    dense_ss60p, df["data"]["id"], df["time"]["avg"]))
